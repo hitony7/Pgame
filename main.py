49d46990a50852e46503018553451485ee87ebff
hitony7 <tywong11@gmail.com>
2019-12-18 00:29:57 -0700

    changed arrangement of code

--- main.py
+++ main.py
@@ -12,20 +12,6 @@ root.resizable(False, False)
 #grid: two dimensional grid
 #place: absolute positioning
 
-windowWidth = 1280
-windowheight = 720
-ButtonWidth = 100 
-ButtonHight = 50
-#xpos get proper center consider the size of the button
-xpos = (windowWidth/2) - (ButtonWidth/2)
-
-startButton = Button(root, text = "Start Game")
-startButton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y=300)
-loadButton = Button(root, text = "Load Picture")
-loadButton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y= 400)
-exitbutton= Button(root, text = "Exit")
-exitbutton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y= 500)
-
 #function that handle button press
 def startGame():
     print("Run Game")
@@ -35,5 +21,20 @@ def loadPic():
 
 def exitGame():
     print("Exit")
+
+windowWidth = 1280
+windowheight = 720
+ButtonWidth = 100 
+ButtonHight = 50
+#xpos get proper center consider the size of the button
+xpos = (windowWidth/2) - (ButtonWidth/2)
+
+startButton = Button(root, text = "Start Game", command=startGame)
+startButton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y=300)
+loadButton = Button(root, text = "Load Picture", command=loadPic)
+loadButton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y= 400)
+exitbutton= Button(root, text = "Exit",command=exitGame)
+exitbutton.place(height=ButtonHight, width=ButtonWidth, x=xpos, y= 500)
+
  
 root.mainloop()
