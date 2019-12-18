7c3f178982e7dae9b8019f2532a2d9ea01ca19d8
hitony7 <tywong11@gmail.com>
2019-12-17 23:31:03 -0700

    Basic tkinter template

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+#using tkinter which use the concept of widgets
+
+#This the root widget(The Window)
+root = Tk()
+root.title("Flappy Bird")
+root.geometry("1280x720")
+root.resizable(False, False)
+
+#tkinter has 3 layout managers
+#pack:  horizontal and vertical boxes
+#grid: two dimensional grid
+#place: absolute positioning
+
+ 
+root.mainloop()
